8fab8a1c715be2e30b587615da3a94e29fe23db4
luobotang <tangkangxing@sina.com>
2015-08-22 22:38:26 +0800

    v0.4.4 start server in new window and log request url

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic('app')).listen(8080);
+var app = require('connect')()
+var serveStatic = require('serve-static')
+
+var PORT = 8080
+
+app.use(function (req, res, next) {
+	console.log(new Date().toISOString() + ' request for url: ' + req.url)
+	next()
+}).use(serveStatic('app', {index: ['index.htm']})).listen(PORT)
+
+console.log('server started on port: ' + PORT)
