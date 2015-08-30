babaf7d2f424b99db2e08ded3599526403a3b577
luobotang <tangkangxing@sina.com>
2015-08-30 13:00:09 +0800

    simplify log info

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static')
 var PORT = 8080
 
 app.use(function (req, res, next) {
-	console.log(new Date().toISOString() + ' request for url: ' + req.url)
+	console.log(decodeURI(req.url))
 	next()
 }).use(serveStatic('app', {index: ['index.htm']})).listen(PORT)
 
